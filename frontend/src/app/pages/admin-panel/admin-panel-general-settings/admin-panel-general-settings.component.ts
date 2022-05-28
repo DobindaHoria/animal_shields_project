2228d03126cb3b1b9bea9b1456f63a975bab6ee2
Dobinda horia <dobindahoria@gmail.com>
2022-05-28 19:10:02 +0300

    feat(admin-gen-sett): add general settings + create new tag

--- frontend/src/app/pages/admin-panel/admin-panel-general-settings/admin-panel-general-settings.component.ts
+++ frontend/src/app/pages/admin-panel/admin-panel-general-settings/admin-panel-general-settings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { RequestService } from '../../../services/request.service'
+import { environment } from '../../../../../src/environments/environment';
+declare var $: any;
 
 @Component({
   selector: 'app-admin-panel-general-settings',
@@ -6,10 +10,98 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-panel-general-settings.component.scss']
 })
 export class AdminPanelGeneralSettingsComponent implements OnInit {
+  token: any = ''
 
-  constructor() { }
+  settingsModel: any = {
+    message: "",
+    error: "",
+    value: null
+  }
+
+  updateSettingsModel: any = {
+    message: "",
+    error: "",
+    value: null
+  }
+
+  arrayOfROTags: any = []
+  arrayOfENTags: any = []
+
+  selectedTag: any = {
+    language: '',
+    index: '',
+    name: '',
+  }
+
+  constructor(
+    private location: Location,
+    private requestService: RequestService
+  ) { }
 
   ngOnInit(): void {
+    if (localStorage.getItem('accessToken')) this.token = localStorage.getItem('accessToken')
+    this.getAllSettings()
+  }
+
+  onNavigateBack() {
+    this.location.back();
+  }
+
+  getAllSettings() {
+    return this.requestService.requestGet(`${environment.apiUrl}/settings`, this.settingsModel, { "Authorization": `Bearer ${this.token}` }, () => {
+      if (this.settingsModel.value.Article_tags) {
+        console.log(this.settingsModel.value.Article_tags)
+        for (let item of this.settingsModel.value.Article_tags) {
+          if (item.language === 'ro') {
+            this.arrayOfROTags = item.tags
+          } else {
+            this.arrayOfENTags = item.tags
+          }
+        }
+      }
+    })
+  }
+
+  onUpdateTags() {
+    let newTags = this.selectedTag.language === 'ro' ? [...this.arrayOfROTags] : [...this.arrayOfENTags]
+
+    console.log(this.selectedTag);
+
+    if (this.selectedTag.index !== null) {
+      console.log('NewTags', newTags);
+      console.log('this.selectedTag.index', this.selectedTag.index);
+
+      newTags[this.selectedTag.index] = this.selectedTag.name
+    }
+
+    let body = {
+      language: this.selectedTag.language,
+      newTags: newTags
+    }
+
+    return this.requestService.requestPut(`${environment.apiUrl}/settings`, this.updateSettingsModel, body, { "Authorization": `Bearer ${this.token}` }, () => {
+      if (this.updateSettingsModel.message === 'Procesul a fost executat cu succes' || this.updateSettingsModel.message === 'Process completed successfully.') {
+        setTimeout(() => {
+          $('#editTagModal').modal('hide');
+          this.selectedTag = {
+            language: '',
+            index: '',
+            name: '',
+          }
+          this.getAllSettings()
+        }, 1500)
+      }
+    })
+  }
+
+  onOpenEditModalTag(tag: any, indexOfTag: any, language: any) {
+    this.selectedTag = {
+      language: language,
+      index: indexOfTag,
+      name: tag,
+    }
+    this.updateSettingsModel.message = ''
+    this.updateSettingsModel.error = ''
   }
 
 }
